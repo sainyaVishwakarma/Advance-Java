1189e3105f4dde892b417b677a51d0ae3d03699a
sainyaVishwakarma <104253247+sainyaVishwakarma@users.noreply.github.com>
2023-07-03 18:11:51 +0530

    Add files via upload

---
+++ MyFirstServlet/src/main/java/com/java/servlet/MyServlet.java
@@ -0,0 +1,18 @@
+package com.java.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet{
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("<h3>helloworld!!</h3>");
+		out.println("<h3>my first running servlet...</h3>");
+	}
+
+}
